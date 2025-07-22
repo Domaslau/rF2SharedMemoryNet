0e6ba020f7c88c4aeaa2d9ae4c8ad30e3b741750
Domas <laur.domas@proton.me>
2025-07-22 14:06:44 +0100

    Fixed mistake in slip angle offset. Bump version 1.0.4

--- rF2SharedMemoryNet/LMUData/Constants/MemoryAddressOffsets.cs
+++ rF2SharedMemoryNet/LMUData/Constants/MemoryAddressOffsets.cs
@@ -9,7 +9,7 @@ namespace rF2SharedMemoryNet.LMUData.Constants
     internal static class MemoryAddressOffsets
     {
         public static readonly int TcOffset = 0x1A0DC14;
-        public static readonly int TcSlipOffset = 0x1A0DC34;
+        public static readonly int TcSlipOffset = 0x1A0DD34;
         public static readonly int TcCutOffset = 0x1A0DCA4;
         public static readonly int AntiLockBrakesOffset = 0x1A0DDC4;
         public static readonly int EngineMapOffset = 0x1A1680C;
